352f5da4d9ecdcddce40f8845aabd9b8ed0b9063
BARLIER Kilian <kilian.barlier@insep.fr>
2025-10-31 18:59:52 +0100

    retrait des packages R déjà installés par défaut

--- R/r-install-packages.sh
+++ R/r-install-packages.sh
@@ -3,7 +3,7 @@
 # Installer des packages R à partir d'un fichier requirements
 
 REPO_GIT=datalab-init-scripts
-git clone --depth 1 https://github.com/France-Sport/${GIT_REPO}.git
-chown -R onyxia:users ${GIT_REPO}/
-cd ${GIT_REPO}
+git clone --depth 1 https://github.com/France-Sport/${REPO_GIT}.git
+chown -R onyxia:users ${REPO_GIT}/
+cd ${REPO_GIT}
 Rscript -e "install.packages(readLines('inputs/requirements-r'), Ncpus = 2)"
--- python/py-install-packages.sh
+++ python/py-install-packages.sh
@@ -3,7 +3,7 @@
 # Installer des packages python à partir d'un fichier requirements
 
 REPO_GIT=datalab-init-scripts
-git clone --depth 1 https://github.com/France-Sport/${GIT_REPO}.git
-chown -R onyxia:users ${GIT_REPO}/
-cd ${GIT_REPO}
+git clone --depth 1 https://github.com/France-Sport/${REPO_GIT}.git
+chown -R onyxia:users ${REPO_GIT}/
+cd ${REPO_GIT}
 pip install -r inputs/requirements-py
